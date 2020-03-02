517ef60dcc29e06be97ba104c068d74b03b9bdf2
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2020-03-02 14:54:33 +0100

    Hello People

---
+++ Script/index.js
@@ -0,0 +1,31 @@
+// let loader = document.getElementById("loader");
+// let start = document.getElementById("start");
+// let resume = document.getElementById("res");
+
+
+
+// resume = () => {
+
+// resume.removeChild("res");
+
+// };
+// praise = () => {
+    // setTimeout(() => {
+    // loader.classList.add("hidden");
+    // start.classList.remove("hidden");
+        // loader.classList.add("hidden");
+    // start.classList.remove("hidden");
+    // loader.classList.add("hidden");
+    // }, 5000);
+    // setTimeout(() => {
+    //     start.classList.remove("hidden");
+    //     loader.classList.add("hidden");
+    // }, 1000);
+    // start.classList.remove("hidden");
+    // loader.classList.add("hidden");
+// };
+// setTimeout(() => {
+
+// start.classList.remove("hidden");
+// loader.classList.add("hidden");
+// }, 1000000);
